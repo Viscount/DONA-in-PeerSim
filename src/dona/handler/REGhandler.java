486c7dc58dc823a792261b17cbf29e199b7d4e1b
Viscount <maple01@126.com>
2015-01-17 22:58:35 +0800

    handler for REG (unfinished)

--- src/dona/handler/REGhandler.java
+++ src/dona/handler/REGhandler.java
@@ -2,13 +2,16 @@ package dona.handler;
 
 import peersim.core.Node;
 import dona.entity.Message;
+import dona.protocol.Infrastructure;
 
 public class REGhandler extends Handler{
 
 	@Override
 	public void handleMessage(Node node, int protocolID, Message message) {
 		// TODO Auto-generated method stub
+		Infrastructure inf = (Infrastructure) node.getProtocol(protocolID);
 		
+		inf.fib.addItem(message.getDataName(), message.getRequester());
 	}
 
 }
