d0b9a94b38c010eedace575e2a51a13e8c3b807e
Viscount <maple01@126.com>
2015-01-19 10:44:55 +0800

    add handler for REQ

--- src/dona/handler/REQhandler.java
+++ src/dona/handler/REQhandler.java
@@ -1,6 +1,9 @@
 package dona.handler;
 
+import peersim.config.FastConfig;
+import peersim.core.Network;
 import peersim.core.Node;
+import peersim.transport.Transport;
 import dona.entity.Message;
 import dona.protocol.Infrastructure;
 
@@ -11,7 +14,36 @@ public class REQhandler extends Handler{
 		// TODO Auto-generated method stub
 		Infrastructure inf = (Infrastructure) node.getProtocol(protocolID);
 		
-		
+		if ( (int)message.getInfo("SourceID") == node.getIndex() ){
+			if (inf.contentStore.containsKey(message.getDataName())){
+				// generate DAT
+				Message dat_message = new Message("DAT",node.getIndex(),message.getDataName());
+				dat_message.insertInfo("ChunkNo", message.getInfo("ChunkNo"));
+				dat_message.insertInfo("SourceID", (int)node.getID());
+				dat_message.insertInfo("RequesterID", message.getInfo("RequesterID"));
+				((Transport)node.getProtocol(FastConfig.getTransport(protocolID))).
+				send(node, Network.get(message.getRequester()), dat_message, protocolID);
+			}
+		}
+		else {
+			if (inf.pit.containsKey(message.getDataName()+","+message.getInfo("ChunkNo"))){
+				inf.pit.addFace(message.getDataName()+","+message.getInfo("ChunkNo"), message.getRequester(), 1);
+			}
+			else {
+				inf.pit.addEntry(message.getDataName()+","+message.getInfo("ChunkNo"), message.getRequester(), 1);
+				if (inf.fib.containsKey(message.getDataName())){
+					int nexthop = inf.fib.getNextHop(message.getDataName(), (int)message.getInfo("SourceID"));
+					try {
+						Message req_mess = message.clone(node.getIndex());
+						((Transport)node.getProtocol(FastConfig.getTransport(protocolID))).
+						send(node, Network.get(nexthop), req_mess, protocolID);
+					} catch (CloneNotSupportedException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+			}
+		}
 	}
 
 }
